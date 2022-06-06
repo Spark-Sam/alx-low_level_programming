c0a6e75ac4827161907c7f2f54e60d597d5423a0
Samuel Mwangi <sam.kichwa@gmail.com>
2022-06-06 15:12:13 -0700

    All the single digits, all the single digits

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: All single digits
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int digit;
+
+	for (digit = -1; digit < 9; digit++)
+	{
+		printf(digit,"\n");
+	}
+
+	return (0);
+}
