1be304c80c03d7ac58c9aac82ba67b91177b9911
Samuel Mwangi <sam.kichwa@gmail.com>
2022-06-06 14:03:57 -0700

    Print out the alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: print the alphabet
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	char alphabet;
+
+	for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
+	{
+		putchar(alphabet);
+	}
+
+	putchar('\n');
+
+	return (0);
+}
