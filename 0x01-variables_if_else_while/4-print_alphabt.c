877c7bc0b312d47ff15794f16407bd674f3da892
Samuel Mwangi <sam.kichwa@gmail.com>
2022-06-06 15:00:37 -0700

    Alphabet except 'q' and 'e'

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: Print alphabet except 'q' and 'e'
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	char alphabet;
+	for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
+	{
+		if (alphabet != 'q' && alphabet != 'e')
+			putchar(alphabet);
+	}
+
+	putchar('\n');
+
+	return (0);
+}
