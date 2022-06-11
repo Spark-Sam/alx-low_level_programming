cdd73977c24edc4e9926970d2cc4dc718e7d1cf2
Samuel Mwangi <sam.kichwa@gmail.com>
2022-06-11 07:12:00 -0700

    Its simply low level code. C

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -6,7 +6,7 @@
  *
  * Description: Program that prints _putchar, followed by a new line
  *
- * Return: 0 
+ * Return: 0
  */
 
 int main(void)
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -3,10 +3,11 @@
 
 /*
  * File: main.h
- * Auth: Julius Gathama
+ * Auth: Samuel Mwangi
  * Desc: Header file containing declarations for all functions
  *       used in the 0x02-functions_nested_loops directory.
  */
+
 int _putchar(char c);
 void print_alphabet(void);
 void print_alphabet_x10(void);
